0bd78e41cffeda19b19bf82ffd339d0b288f2390
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2017-05-04 14:21:31 +0100

    Unless unflipped, the table remains flipped

--- app/tableflip.rb
+++ app/tableflip.rb
@@ -1,12 +1,17 @@
 module TableFlip
   extend Discordrb::Commands::CommandContainer
   
+  isUnFlipped = true
+  
   command :tableflip, description: "For those mobile users to flip tables too." do |event|
+    break unless isUnFlipped
     event.respond "(╯°□°）╯︵ ┻━┻"
+    isUnFlipped = false
   end
   
   command :unflip, description: "Unflip those flipped tables. Poor table-kun" do |event|
     event.respond "┬─┬﻿ ノ( ゜-゜ノ)"
+    isUnFlipped = true
   end
   
   command :shrug, description: "*shrugs*" do |event|
