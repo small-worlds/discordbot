d7d43498a53db5b49f413c85604111d0f46df616
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2017-05-04 14:29:27 +0100

    &tableflip now recognises its state.

--- app/tableflip.rb
+++ app/tableflip.rb
@@ -4,14 +4,25 @@ module TableFlip
   isUnFlipped = true
   
   command :tableflip, description: "For those mobile users to flip tables too." do |event|
-    break unless isUnFlipped
-    event.respond "(╯°□°）╯︵ ┻━┻"
-    isUnFlipped = false
+    if isUnFlipped
+      event.respond "(╯°□°）╯︵ ┻━┻"
+      isUnFlipped = false
+      break
+    else
+      event.respond "table already flipped ┻━┻"
+      break
+    end
   end
   
   command :unflip, description: "Unflip those flipped tables. Poor table-kun" do |event|
-    event.respond "┬─┬﻿ ノ( ゜-゜ノ)"
-    isUnFlipped = true
+    if isUnFlipped
+      event.respond "table already upright ┬─┬"
+      break
+    else
+      event.respond "┬─┬﻿ ノ( ゜-゜ノ)"
+      isUnFlipped = true
+      break
+    end
   end
   
   command :shrug, description: "*shrugs*" do |event|
