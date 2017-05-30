4866aafbb54707651684a6a5cc8be639a36d06fe
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2017-05-30 12:08:09 +0100

    renamed variables

--- app/emoji_move.rb
+++ app/emoji_move.rb
@@ -2,26 +2,26 @@ module EmojiRole
   extend Discordrb::EventContainer
   
   reaction_add(emoji: 236127165010280458) do |event|
-    dave=nil
+    alice=nil
     if event.message.id == 316293235700137984
-      dave=event.user.on(191725064155168770)
-      dave.add_role(308279505091559434)
+      alice=event.user.on(191725064155168770)
+      alice.add_role(308279505091559434)
     end
   end
   
   reaction_add(emoji: 316295019097227264) do |event|
-    dave=nil
+    barry=nil
     if event.message.id == 316293235700137984
-      dave=event.user.on(191725064155168770)
-      dave.add_role(308593158827278337)
+      barry=event.user.on(191725064155168770)
+      barry.add_role(308593158827278337)
     end
   end
   
   reaction_add(emoji: 316295046704136202) do |event|
-    dave=nil
+    candy=nil
     if event.message.id == 316293235700137984
-      dave=event.user.on(191725064155168770)
-      dave.add_role(308279505091559434)
+      candy=event.user.on(191725064155168770)
+      candy.add_role(308279505091559434)
     end
   end
 end
