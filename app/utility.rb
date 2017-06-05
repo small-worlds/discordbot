633164b1d8a9039a9f4c4857cb47f5e268892dfb
Viljo Viitanen <viljo.viitanen@gmail.com>
2017-06-05 22:54:46 +0300

    hopefully more clear code for tracking

--- app/utility.rb
+++ app/utility.rb
@@ -1,34 +1,21 @@
 module Utility
   extend Discordrb::EventContainer
   
+  swebotoutput = 321347260099198986 #swebot-output
+
   member_join do |event|
-    channel1 = nil
-    # Object
-    channel2 = 321347260099198986
-    # Integer
-    output = event.user.username + " joined the server"
-    # Output message
-    event.server.channels.each do |event|
-    # For each of the channels on this server, do the following:
-      if event.id == channel2
-      # Does it's id match channel2's?
-        channel1 = event
-        # If so, give the object to channel1
+    event.server.channels.each do |ch|
+      if ch.id == swebotoutput
+        ch.send(event.user.username + " joined the server")
       end
     end
-    channel1.send(output)
-    # Send the contents of output to channel1 
   end
   
   member_leave do |event|
-    channel1 = nil
-    channel2 = 321347260099198986
-    output = event.user.username + " left the server"
-    event.server.channels.each do |event|
-      if event.id == channel2
-        channel1 = event
+    event.server.channels.each do |ch|
+      if ch.id == swebotoutput
+        ch.send(event.user.username + " left the server")
       end
     end
-    channel1.send(output)
   end
 end
