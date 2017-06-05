2e97f0a59e29823aeb92b6222f3c2f7224bb6c89
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2017-06-05 19:19:11 +0100

    On member_leave, also send a message to tracking channel

--- app/utility.rb
+++ app/utility.rb
@@ -20,5 +20,22 @@ module Utility
     # Send the contents of output to channel12 
   end
   
-  
+  member_leave do |event|
+    channel14 = nil
+    # Object
+    channel15 = 321347260099198986
+    # Interger
+    output = event.user.username + " left the server"
+    # Output message
+    event.server.channels.each do |carl|
+    # For each of the channels on this server, do the following:
+      if carl.id == channel15
+      # Does it's id match channel15's?
+        channel14 = carl
+        # If so, give the object to channel14
+      end
+    end
+    channel14.send(output)
+    # Send the contents of output to channel14 
+  end
 end
