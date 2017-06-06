ea4bb2b9b96e81decf4bb39d19f170ff4db88924
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2017-06-07 00:56:45 +0100

    the list now works off nicks, not account names

--- app/lists.rb
+++ app/lists.rb
@@ -19,10 +19,15 @@ module Listing
     end
   end
   
-  command :wingremove, required_roles: [Roles::Operators], description: "Remove a person from the wing queue", min_args: 1, max_args: 1 do |event, target|
+  command :wingremove, required_roles: [Roles::Operators], description: "Remove a person from the wing waiting list", min_args: 1, max_args: 1 do |event, target|
   # Remove someone who is on the wing list. Needs operator role.
     wing_list.delete(target)
-    event.respond "Removed #{target} from the queue."
+    event.respond "Removed #{target} from the waiting list."
+  end
+  
+  command :wingcancel, description: "Removes the user from the waiting list." do |event|
+    wing_list.delete(event.author.display_name)
+    event.respond "You have been removed from the waiting list."
   end
 
   command :wingnuke, required_roles: [Roles::Operators], description: "Removes everything from the wing list. Useful for after a meetup." do |event|
@@ -32,7 +37,7 @@ module Listing
     event.respond "As requested, the list has been nuked."
   end
 
-  command :wingme, description: "Adds you to the wing queue" do |event|
+  command :wingme, description: "Adds you to the wing waiting list" do |event|
   # Adds the person who used the command to wing_list.
     if wing_switch == false
     # Turns the command on and off
@@ -41,14 +46,14 @@ module Listing
     else
       break if event.channel.name.downcase != 'bot-abuse'
       # Forces people to only use bot-abuse for wing requests.
-      if wing_list.include?(event.user.name)
+      if wing_list.include?(event.author.display_name)
       # Check the user isn't spamming their name onto the list...
         event.respond "You're already on this list! Be patient!"
         # ...and tell them off for it.
       else
-        wing_list << event.user.name
-        # Find the user who typed &WingMe and add them to the array-list.
-        event.respond "Added to the Wing request queue."
+        wing_list << event.author.display_name
+        # Find the user who typed &WingMe and add their nickname to the array-list.
+        event.respond "Added to the Wing request waiting list."
       end
     end
   end
@@ -58,7 +63,7 @@ module Listing
       # Save first value to tag_me
       tag_me = wing_list.shift
       if tag_me.nil?
-        event.respond "There is currently no queue."
+        event.respond "There is currently no waiting list."
       end
 
       # Find the member object associated with the member. We could probably do this more gracefully.
