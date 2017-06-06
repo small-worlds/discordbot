34bf457213d42b82e23a4ab5b3580c3f3d4a17b7
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2017-06-07 00:12:40 +0100

    Uhh, yeah, please test. Prevous commit wouldn't run

--- app/emoji_role.rb
+++ app/emoji_role.rb
@@ -9,21 +9,25 @@ module EmojiRole
   reaction_add(emoji: [Emojis::small_worlds]) do |event|
     alice=nil
     if event.message.id == messageid
-      alice=event.author.add_role(Roles::SWE2)
+      alice=event.user.on(191725064155168770)
+      alice.add_role(Roles::SmallWorlds2)
     end
   end
   
   reaction_add(emoji: [Emojis::small_worlds_xbox]) do |event|
     barry=nil
     if event.message.id == messageid
-      barry=event.author.add_role(Roles::SWE2Xbox)
+      barry=event.user.on(191725064155168770)
+      barry.add_role(Roles::SWE2Xbox)
     end
   end
   
-  reaction_add(emoji: [Emojis::roleplayers]) do |event|
+  reaction_add(emoji: 316295046704136202) do |event|
     candy=nil
     if event.message.id == messageid
-      candy=event.author.add_role(Roles::Roleplayers)
+      candy=event.user.on(191725064155168770)
+      candy.add_role(Roles::Roleplayers)
     end
   end
 end
+Roles::Roleplayers
