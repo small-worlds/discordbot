6c7a145f5a6557502fd221ea84b9b9676b49c931
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2017-06-07 20:54:11 +0100

    Fixed?

--- app/emoji_role.rb
+++ app/emoji_role.rb
@@ -4,11 +4,11 @@ module EmojiRole
   extend Emojis
   
   # Message id we're looking for
-  messageid=316293235700137984
+  MESSAGEID=316293235700137984
   
   reaction_add(emoji: [Emojis::SMALL_WORLDS]) do |event|
     alice=nil
-    if event.message.id == messageid
+    if event.message.id == MESSAGEID
       alice=event.user.on(191725064155168770)
       alice.add_role(Roles::SWE2)
     end
@@ -16,7 +16,7 @@ module EmojiRole
   
   reaction_add(emoji: [Emojis::XBOX]) do |event|
     barry=nil
-    if event.message.id == messageid
+    if event.message.id == MESSAGEID
       barry=event.user.on(191725064155168770)
       barry.add_role(Roles::SWE2Xbox)
     end
@@ -24,7 +24,7 @@ module EmojiRole
   
   reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
     candy=nil
-    if event.message.id == messageid
+    if event.message.id == MESSAGEID
       candy=event.user.on(191725064155168770)
       candy.add_role(Roles::Roleplayers)
     end
