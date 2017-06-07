dafcdf37f0d8adc6fb5b3dd9e8b3d31de9d7e217
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2017-06-07 21:19:43 +0100

    More constants

--- app/emoji_role.rb
+++ app/emoji_role.rb
@@ -2,13 +2,11 @@ module EmojiRole
   extend Discordrb::EventContainer
   extend Roles
   extend Emojis
-  
-  # Message id we're looking for
-  MESSAGEID=316293235700137984
-  
+  extend Message
+
   reaction_add(emoji: [Emojis::SMALL_WORLDS]) do |event|
     alice=nil
-    if event.message.id == MESSAGEID
+    if event.message.id == Message::MESSAGEID
       alice=event.user.on(191725064155168770)
       alice.add_role(Roles::SWE2)
     end
@@ -16,7 +14,7 @@ module EmojiRole
   
   reaction_add(emoji: [Emojis::XBOX]) do |event|
     barry=nil
-    if event.message.id == MESSAGEID
+    if event.message.id == Message::MESSAGEID
       barry=event.user.on(191725064155168770)
       barry.add_role(Roles::SWE2Xbox)
     end
@@ -24,7 +22,7 @@ module EmojiRole
   
   reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
     candy=nil
-    if event.message.id == MESSAGEID
+    if event.message.id == Message::MESSAGEID
       candy=event.user.on(191725064155168770)
       candy.add_role(Roles::Roleplayers)
     end
