2724965de62c1a77d7688d922e702cdb99e3a8f0
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2018-01-06 22:40:38 +0000

    commented out two (old) chunks

--- app/emoji_role.rb
+++ app/emoji_role.rb
@@ -5,21 +5,21 @@ module EmojiRole
   extend Message
   extend Server
 
-  reaction_add(emoji: [Emojis::SMALL_WORLDS]) do |event|
-    alice=nil
-    if event.message.id == Message::MESSAGEID
-      alice=event.user.on(Server::Server)
-      alice.add_role(Roles::SWE2)
-    end
-  end
+  # reaction_add(emoji: [Emojis::SMALL_WORLDS]) do |event|
+    # alice=nil
+    # if event.message.id == Message::MESSAGEID
+      # alice=event.user.on(Server::Server)
+      # alice.add_role(Roles::SWE2)
+    # end
+  # end
   
-  reaction_add(emoji: [Emojis::XBOX]) do |event|
-    barry=nil
-    if event.message.id == Message::MESSAGEID
-      barry=event.user.on(Server::Server)
-      barry.add_role(Roles::SWE2Xbox)
-    end
-  end
+  # reaction_add(emoji: [Emojis::XBOX]) do |event|
+    # barry=nil
+    # if event.message.id == Message::MESSAGEID
+      # barry=event.user.on(Server::Server)
+      # barry.add_role(Roles::SWE2Xbox)
+    # end
+  # end
   
   reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
     candy=nil
