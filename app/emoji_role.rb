871daa85ac42c1bae0d0726105eb5d2291c9e7f0
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2018-01-21 17:17:32 +0000

    Thanks to Vii, this should be more compact now

--- app/emoji_role.rb
+++ app/emoji_role.rb
@@ -4,6 +4,44 @@ module EmojiRole
   extend Emojis
   extend Message
   extend Server
+  
+  def self.addrole(event, role) #Creates a method with two inputs, the event handler and the role to be added
+    if event.message.id == Message::MESSAGEID  #checking the message the emoji is on is the right on
+      event.user.on(Server:Server).add_role(role) #apply role to forehead
+    end
+  end
+
+  reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    addrole(event, Roles::Roleplayers) #Roleplayers
+  end
+  
+  reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    addrole(event, Roles::Jerek) #Unplanned beagle expo
+  end
+  
+  reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    addrole(event, Roles::FlightClub) #DCS
+  end
+  
+  reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    addrole(event, Roles::StarCitizen) #Star Citizen
+  end
+  
+  reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    addrole(event, Roles::Kruger) #SWE Winter expo 3 (Saud Kruger Beacon tour)
+  end
+  
+  # reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    # addrole(event, Roles::Roleplayers) #Unused
+  # end
+  
+  # reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    # addrole(event, Roles::Roleplayers) #Unused
+  # end
+  
+  # reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    # addrole(event, Roles::Roleplayers) #Unused
+  # end
 
   # reaction_add(emoji: [Emojis::SMALL_WORLDS]) do |event|
     # alice=nil
@@ -21,43 +59,43 @@ module EmojiRole
     # end
   # end
   
-  reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
-    candy=nil
-    if event.message.id == Message::MESSAGEID
-      candy=event.user.on(Server::Server)
-      candy.add_role(Roles::Roleplayers)
-    end
-  end
+  # reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    # candy=nil
+    # if event.message.id == Message::MESSAGEID
+      # candy=event.user.on(Server::Server)
+      # candy.add_role(Roles::Roleplayers)
+    # end
+  # end
   
-  reaction_add(emoji: [Emojis::ONIONHEAD]) do |event|
-    delta=nil
-    if event.message.id == Message::MESSAGEID
-      delta=event.user.on(Server::Server)
-      delta.add_role(Roles::Jerek)
-    end
-  end
+  # reaction_add(emoji: [Emojis::ONIONHEAD]) do |event|
+    # delta=nil
+    # if event.message.id == Message::MESSAGEID
+      # delta=event.user.on(Server::Server)
+      # delta.add_role(Roles::Jerek)
+    # end
+  # end
   
-  reaction_add(emoji: [Emojis::PLANE]) do |event|
-    ecchi=nil
-    if event.message.id == Message::MESSAGEID
-      ecchi=event.user.on(Server::Server)
-      ecchi.add_role(Roles::FlightClub)
-    end
-  end
+  # reaction_add(emoji: [Emojis::PLANE]) do |event|
+    # ecchi=nil
+    # if event.message.id == Message::MESSAGEID
+      # ecchi=event.user.on(Server::Server)
+      # ecchi.add_role(Roles::FlightClub)
+    # end
+  # end
   
-  reaction_add(emoji: [Emojis::STARCITIZEN]) do |event|
-    foxtrot=nil
-    if event.message.id == Message::MESSAGEID
-      foxtrot=event.user.on(Server::Server)
-      foxtrot.add_role(Roles::StarCitizen)
-    end
-  end
+  # reaction_add(emoji: [Emojis::STARCITIZEN]) do |event|
+    # foxtrot=nil
+    # if event.message.id == Message::MESSAGEID
+      # foxtrot=event.user.on(Server::Server)
+      # foxtrot.add_role(Roles::StarCitizen)
+    # end
+  # end
   
-  reaction_add(emoji: [Emojis::ORCA]) do |event|
-    hotel=nil
-    if event.message.id == Message::MESSAGEID
-      hotel=event.user.on(Server::Server)
-      hotel.add_role(Roles::Kruger)
-    end
-  end
+  # reaction_add(emoji: [Emojis::ORCA]) do |event|
+    # hotel=nil
+    # if event.message.id == Message::MESSAGEID
+      # hotel=event.user.on(Server::Server)
+      # hotel.add_role(Roles::Kruger)
+    # end
+  # end
 end
