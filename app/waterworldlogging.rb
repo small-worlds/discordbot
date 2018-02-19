6349def5885d8ea498d93d7205077dcb6ec39805
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2018-02-19 20:53:02 +0000

    CSV support and comments on thought train.

--- app/waterworldlogging.rb
+++ app/waterworldlogging.rb
@@ -3,12 +3,12 @@ module WaterWorld
   extend Roles
   extend Channel
   
-  command :log, required_roles: [Roles::Kruger], description: 'Log a found waterworld. Usage: <System> : <Planet code>', usage: "<System> : <Planet code>" do |event, *parse_string|
-    parse_string = parse_string.join(' ')
-    parse_string = parse_string.split(':', 2)
-    system = parse_string[0].gsub(/\s+$/, '')
-    planet_code = parse_string[1].chomp.gsub!(/^\s+/, '')
+  command :log, required_roles: [Roles::Kruger], description: 'Log a found waterworld. Usage: <System> : <Planet code>', usage: "<System> : <Planet code>" do |event, *parse_string| # fyi, this requires the SaudKruiger role.
+    parse_string = parse_string.join(' ') #lane's code
+    parse_string = parse_string.split(':', 2) #splits the string on the ':'
+    system = parse_string[0].gsub(/\s+$/, '') #lane's code, no idea.
+    planet_code = parse_string[1].chomp.gsub!(/^\s+/, '') #also lane's code
     
-    event.bot.send_message(Channel::WaterWorld, "WW found, system: " + system + "\nPlanet: " + planet_code + "\nCommander: " + event.author.display_name)
+    event.bot.send_message(Channel::WaterWorld, event.author.display_name + ","+ system + "," + planet_code) #Sends the display/nickname of the command executor, the value of "system", and the value of "planet_code" to the channel WaterWorld, defined in the module "Channel"
   end
 end
