736b6a259e50323c4fa1e47ac3d6f7596ce681fe
TOASTERMK3-PC\ravstar <ravstargadhia@yahoo.co.uk>
2018-05-11 18:52:07 +0100

    Added both Xbox and PS player's roles

--- app/emoji_role.rb
+++ app/emoji_role.rb
@@ -39,63 +39,24 @@ module EmojiRole
     addrole(event, Roles::SWE3) #Small Worlds Three!
   end
   
+  reaction_add(emoji: [Emojis::XBOX]) do |event|
+    addrole(event, Roles::XBOX) #Xbox
+  end
+  
+  reaction_add(emoji: [Emojis::PS]) do |event|
+    addrole(event, Roles::PS) #PS4
+  end
+  
+  # reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    # addrole(event, Roles::Roleplayers) #Unused
+  # end
+  
+  # reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
+    # addrole(event, Roles::Roleplayers) #Unused
+  # end
+  
   # reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
     # addrole(event, Roles::Roleplayers) #Unused
   # end
 
-  # reaction_add(emoji: [Emojis::SMALL_WORLDS]) do |event|
-    # alice=nil
-    # if event.message.id == Message::MESSAGEID
-      # alice=event.user.on(Server::Server)
-      # alice.add_role(Roles::SWE2)
-    # end
-  # end
-  
-  # reaction_add(emoji: [Emojis::XBOX]) do |event|
-    # barry=nil
-    # if event.message.id == Message::MESSAGEID
-      # barry=event.user.on(Server::Server)
-      # barry.add_role(Roles::SWE2Xbox)
-    # end
-  # end
-  
-  # reaction_add(emoji: [Emojis::ROLEPLAYERS]) do |event|
-    # candy=nil
-    # if event.message.id == Message::MESSAGEID
-      # candy=event.user.on(Server::Server)
-      # candy.add_role(Roles::Roleplayers)
-    # end
-  # end
-  
-  # reaction_add(emoji: [Emojis::ONIONHEAD]) do |event|
-    # delta=nil
-    # if event.message.id == Message::MESSAGEID
-      # delta=event.user.on(Server::Server)
-      # delta.add_role(Roles::Jerek)
-    # end
-  # end
-  
-  # reaction_add(emoji: [Emojis::PLANE]) do |event|
-    # ecchi=nil
-    # if event.message.id == Message::MESSAGEID
-      # ecchi=event.user.on(Server::Server)
-      # ecchi.add_role(Roles::FlightClub)
-    # end
-  # end
-  
-  # reaction_add(emoji: [Emojis::STARCITIZEN]) do |event|
-    # foxtrot=nil
-    # if event.message.id == Message::MESSAGEID
-      # foxtrot=event.user.on(Server::Server)
-      # foxtrot.add_role(Roles::StarCitizen)
-    # end
-  # end
-  
-  # reaction_add(emoji: [Emojis::ORCA]) do |event|
-    # hotel=nil
-    # if event.message.id == Message::MESSAGEID
-      # hotel=event.user.on(Server::Server)
-      # hotel.add_role(Roles::Kruger)
-    # end
-  # end
 end
