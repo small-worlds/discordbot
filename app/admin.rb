ce6c9dbac5866f7860e360d2690794bf78e05e0f
ravstar52 <ravstargadhia@yahoo.co.uk>
2018-05-30 00:22:31 +0100

    Okay, this might work better
    Also the default for wingswitch is now False

--- app/admin.rb
+++ app/admin.rb
@@ -89,8 +89,13 @@ module Admin
   end
   
   command :groupup, required_roles: [Roles::Operators], min_args: 0, help_available: true do |event|
+		event.server.voice_channels.each do |uh|
+			if uh.id == Channel::Conference
+				channel1=uh
+			end
+		end
 		WingSwitch.constants.each do |c|
-			event.server.move(event.server.member(c), Channel::Conference)
+			event.server.move(event.server.member(c), channel1)
 		end
   end
   
