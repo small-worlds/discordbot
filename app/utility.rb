fc961fbd330d062dea7cf8598ae9847d1eee909f
ravstar52 <ravstargadhia@yahoo.co.uk>
2018-10-01 23:21:21 +0100

    Update utility.rb

--- app/utility.rb
+++ app/utility.rb
@@ -8,9 +8,6 @@ module Utility
 
   member_join do |event|
     event.bot.send_message(Channel::Swebotoutput, event.user.username + " joined the server")
-    if{event.user.username.include? "discord.gg"
-      server.kick(event.user)
-    }
   end
   
   member_leave do |event|
